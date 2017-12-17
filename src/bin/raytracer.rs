e083c43ea6874ae75cb9eb4833dd41acb4141bb0
Coeuvre Wong <coeuvre@gmail.com>
2017-12-17 15:59:15 +0800

    Ray sphere hit

--- src/bin/raytracer.rs
+++ src/bin/raytracer.rs
@@ -3,11 +3,12 @@ extern crate fcg;
 use std::os::raw::c_void;
 use std::ffi::CString;
 use fcg::stb_image_write::stbi_write_png;
+use fcg::math::*;
 
 pub struct ImageBuffer {
     pub width: u32,
     pub height: u32,
-    data: Vec<u8>,
+    data: Vec<u32>,
 }
 
 impl ImageBuffer {
@@ -15,23 +16,14 @@ impl ImageBuffer {
         ImageBuffer {
             width,
             height,
-            data: vec![0; (width * height * 4) as usize],
+            data: vec![0; (width * height) as usize],
         }
     }
 
-    pub fn set_pixel(&mut self, x: u32, y: u32, r: u8, g: u8, b: u8, a: u8) {
+    pub fn set_pixel(&mut self, x: u32, y: u32, color: u32) {
         *self.data
-             .get_mut((y * self.width * 4 + x * 4 + 0) as usize)
-             .unwrap() = r;
-        *self.data
-             .get_mut((y * self.width * 4 + x * 4 + 1) as usize)
-             .unwrap() = g;
-        *self.data
-             .get_mut((y * self.width * 4 + x * 4 + 2) as usize)
-             .unwrap() = b;
-        *self.data
-             .get_mut((y * self.width * 4 + x * 4 + 3) as usize)
-             .unwrap() = a;
+             .get_mut((y * self.width + x) as usize)
+             .unwrap() = color;
     }
 
     pub fn save(&self, path: &str) {
@@ -41,12 +33,42 @@ impl ImageBuffer {
                            self.height as i32,
                            4,
                            self.data.as_ptr() as *const c_void,
-                           (self.width * 4) as i32);
+                           (4 * self.width) as i32);
         }
     }
 }
 
+pub struct Camera {
+    p: Vec3,
+}
+
+impl Camera {
+    pub fn new(p: Vec3) -> Camera {
+        Camera { p }
+    }
+
+    pub fn calc_camera_ray(&self, ib: &ImageBuffer, x_pixel: u32, y_pixel: u32) -> Ray3 {
+        let p = Vec3::new(x_pixel as f32, (ib.height - y_pixel - 1) as f32, 0.0);
+        Ray3::new(self.p, (p - self.p).normalize())
+    }
+}
+
 fn main() {
-    let ib = ImageBuffer::new(1280, 720);
+    const BACKGROUND_COLOR: u32 = 0xFF000000;
+    let mut ib = ImageBuffer::new(1920, 1080);
+    let camera = Camera::new(Vec3::new(960.0, 540.0, -100.0));
+    let sphere = Sphere::new(Vec3::new(960.0, 540.0, 100.0), 100.0);
+
+    for y in 0..ib.height {
+        for x in 0..ib.width {
+            let camera_ray = camera.calc_camera_ray(&ib, x, y);
+            if camera_ray.hit_sphere(&sphere).len() > 0 {
+                ib.set_pixel(x, y, 0xFFFFFFFF);
+            } else {
+                ib.set_pixel(x, y, BACKGROUND_COLOR);
+            }
+        }
+    }
+
     ib.save("output.png");
 }
--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,6 @@
+use std::ops::{Add, Sub, Mul, Div};
+
+#[derive(Clone, Copy)]
 pub struct Vec3 {
     pub x: f32,
     pub y: f32,
@@ -8,15 +11,108 @@ impl Vec3 {
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { x, y, z }
     }
+
+    pub fn normalize(self) -> Vec3 {
+        let len = self.len();
+        self / len
+    }
+
+    pub fn len(&self) -> f32 {
+        self.len_squre().sqrt()
+    }
+
+    pub fn len_squre(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = f32;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(self * rhs.x, self * rhs.y, self * rhs.z)
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Vec3::new(self.x / rhs, self.y / rhs, self.z / rhs)
+    }
 }
 
 pub struct Ray3 {
-    p: Vec3,
+    e: Vec3,
     d: Vec3,
 }
 
 impl Ray3 {
-    pub fn new(p: Vec3, d: Vec3) -> Ray3 {
-        Ray3 { p, d }
+    pub fn new(e: Vec3, d: Vec3) -> Ray3 {
+        assert!(1.0 - d.len_squre() < 0.00001);
+        Ray3 { e, d }
+    }
+
+    pub fn hit_sphere(&self, sphere: &Sphere) -> Vec<f32> {
+        let delta = self.e - sphere.c;
+        let a = self.d * self.d;
+        let b = 2.0 * (self.d * delta);
+        let c = delta * delta - sphere.r * sphere.r;
+        solve_quadratic_equations(a, b, c)
+    }
+
+    pub fn at(&self, t: f32) -> Vec3 {
+        self.e + t * self.d
+    }
+}
+
+pub fn solve_quadratic_equations(a: f32, b: f32, c: f32) -> Vec<f32> {
+    let mut solve = Vec::new();
+
+    let d = b * b - 4.0 * a * c;
+
+    if d == 0.0 {
+        solve.push(-b / (2.0 * a));
+    } else if d > 0.0 {
+        let d_sqrt = d.sqrt();
+        solve.push((-b + d_sqrt) / (2.0 * a));
+        solve.push((-b - d_sqrt) / (2.0 * a));
+    }
+
+    solve
+}
+
+pub struct Sphere {
+    c: Vec3,
+    r: f32,
+}
+
+impl Sphere {
+    pub fn new(c: Vec3, r: f32) -> Sphere {
+        Sphere { c, r }
     }
 }
